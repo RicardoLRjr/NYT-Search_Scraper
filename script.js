606613558dcf894cb64c28fb1ef071d589d912e4
Zack Hayden <signbarn@gmail.com>
2020-03-16 13:41:00 -0400

    adding some pseudocode and fleshing it out a bit more

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 
 $(document).ready(function() {
 
+    var today = moment.now().format("YYYYMMDD");
+    var beginDate, endDate;
 
-    // https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
+    dataObject = {
+        "api-key": "unoOYsH6b5BnSh8amNDBbNKY1K7xZc72",
+        end_date: endDate || today
+    };
+
+    $("#form").on("submit", function() {
+        // gather the input from the input fields
+        // and load them into our data object
+        // dataObject.begin_date: = $("#begin-date").val()
+    });
 
     $.ajax({
         url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
         method: "GET",
-        data: {
-            q: "election",
-            fr: "The New York Times"
-            "api-key": "unoOYsH6b5BnSh8amNDBbNKY1K7xZc72"
-        }
+        data: dataObject
+        // data: {
+        //     q: "election",
+        //     // fr: "The New York Times" // i don't *think* this is necessary
+        //     "api-key": "unoOYsH6b5BnSh8amNDBbNKY1K7xZc72",
+        //     begin_date: "20100101",
+        //     end_date: "20200316"
+        // }
     }).then(function(response) {
         console.log(response);
     });
